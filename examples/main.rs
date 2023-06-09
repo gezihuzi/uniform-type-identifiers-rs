23afc0091c8c1fcb0a121ef048502eeed32c04a7
gezihuzi <a495008890@live.com>
2023-06-09 23:24:21 +0800

    feat: Add example

--- examples/main.rs
+++ examples/main.rs
@@ -1,12 +1,72 @@
-use uniform_type_identifiers::{system_defined::{self, PUBLIC_CALENDAR_EVENT}, UTType};
+use uniform_type_identifiers::system_defined::{
+    PUBLIC_DATA, PUBLIC_ITEM, PUBLIC_JSON, PUBLIC_TEXT, PUBLIC_XML,
+};
 
 fn main() {
-    let raw = system_defined::COM_APPLE_DESKTOP_FOLDER;
-    let folder = UTType::from(raw);
-    let data: UTType = "public.data".into();
-    let json = PUBLIC_CALENDAR_EVENT;
-    let mime_type = json.preferred_mime_type();
-    let extension = json.preferred_filename_extension();
-    println!("{:?}, {:?}", folder, data);
-    println!("{:?}, {:?}, {:?}", json, mime_type, extension);
+    let mime_type = PUBLIC_JSON.preferred_mime_type().unwrap_or_default();
+    let extension = PUBLIC_JSON
+        .preferred_filename_extension()
+        .unwrap_or_default();
+    let super_types = PUBLIC_JSON.super_types();
+    println!("Json Identifier: {:?}", PUBLIC_JSON.identifier());
+    println!("Json MIME type: {:?}", mime_type);
+    println!("Json filename extension: {:?}", extension);
+    println!(
+        "Json super types: {:?}",
+        super_types
+            .iter()
+            .map(|it| it.identifier())
+            .collect::<Vec<_>>()
+    );
+    println!("-----");
+    println!(
+        "Json is conforms data: {:}",
+        PUBLIC_JSON.is_conforms(&PUBLIC_DATA)
+    );
+    println!(
+        "Json is conforms xml: {:}",
+        PUBLIC_JSON.is_conforms(&PUBLIC_XML)
+    );
+    println!("-----");
+    println!(
+        "Json is text supertype: {:}",
+        PUBLIC_JSON.is_supertype(&PUBLIC_TEXT)
+    );
+    println!(
+        "Json is json supertype: {:}",
+        PUBLIC_JSON.is_supertype(&PUBLIC_JSON)
+    );
+    println!(
+        "Text is item supertype: {:}",
+        PUBLIC_TEXT.is_supertype(&PUBLIC_ITEM)
+    );
+    println!(
+        "Text is json supertype: {:}",
+        PUBLIC_TEXT.is_supertype(&PUBLIC_JSON)
+    );
+    println!(
+        "Item is text supertype: {:}",
+        PUBLIC_ITEM.is_supertype(&PUBLIC_TEXT)
+    );
+    println!("-----");
+    println!(
+        "Json is text subtype: {:}",
+        PUBLIC_JSON.is_subtype(&PUBLIC_TEXT)
+    );
+    println!(
+        "Json is json subtype: {:}",
+        PUBLIC_JSON.is_subtype(&PUBLIC_JSON)
+    );
+    println!(
+        "Text is item subtype: {:}",
+        PUBLIC_TEXT.is_subtype(&PUBLIC_ITEM)
+    );
+    println!(
+        "Text is json subtype: {:}",
+        PUBLIC_TEXT.is_subtype(&PUBLIC_JSON)
+    );
+    println!(
+        "Item is text subtype: {:}",
+        PUBLIC_ITEM.is_subtype(&PUBLIC_TEXT)
+    );
 }
